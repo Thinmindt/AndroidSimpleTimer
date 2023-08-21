1738e4acb653423757e76db09ff48de544c3780b
Greg Harms <greg@plotsandschemes.com>
2023-08-21 16:53:13 -0700

    Add timer view, delete timer, edit timer name

--- app/src/main/java/com/example/simpletimer/MainActivity.kt
+++ app/src/main/java/com/example/simpletimer/MainActivity.kt
@@ -3,37 +3,151 @@ package com.example.simpletimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.simpletimer.ui.theme.SimpleTimerTheme
+import java.time.Duration
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel: MainActivityViewModel by viewModels()
+
+
         setContent {
             SimpleTimerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LazyColumn {
+                        items(items = viewModel.timerList) { timer ->
+                            TimerCard(viewModel, timer)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun TimerCard(viewModel: MainActivityViewModel, timer: Timer) {
+    val focusRequester = remember { FocusRequester() }
+
+    var editingName by remember { mutableStateOf(false) }
+
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(2.dp)
+        .pointerInput(Unit) {
+            detectTapGestures(onTap = {
+                editingName = false
+            })
+        }) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            if (editingName) {
+                BasicTextField(
+                    value = timer.name.value,
+                    onValueChange = { timer.name.value = it },
+                    textStyle = MaterialTheme.typography.titleMedium,
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(onDone = {
+                        editingName = false
+                    }),
+                    modifier = Modifier.focusRequester(focusRequester)
+                )
+                LaunchedEffect(Unit) {
+                    focusRequester.requestFocus()
+                }
+            } else {
+                ClickableText(AnnotatedString(timer.name.value),
+                    style = MaterialTheme.typography.titleMedium,
+                    onClick = {
+                        editingName = true
+                    })
+                Button(
+                    onClick = { viewModel.deleteTimer(timer) },
+                    modifier = Modifier.size(25.dp),
+                    contentPadding = PaddingValues(0.dp),  //avoid the little icon
+                    shape = CircleShape
+                ) {
+                    Icon(
+                        Icons.Rounded.Close,
+                        modifier = Modifier.size(20.dp),
+                        contentDescription = "Remove timer"
+                    )
+                }
+            }
+        }
+        Row(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+        ) {
+            Text(timer.time.toDisplayString(), style = MaterialTheme.typography.titleLarge)
+        }
+    }
+}
+
+private fun Duration.toDisplayString(): String {
+    return "${this.toHours()}:${this.toMinutes() % 60}:${seconds % 60}"
+}
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
